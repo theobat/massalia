96e600a2e53ff5e408d300cb0532ba764316314f
Theophile Batoz <theophile.batoz@gmail.com>
2020-04-23 14:24:59 +0200

    fix: import missing

--- src/Massalia/Auth.hs
+++ src/Massalia/Auth.hs
@@ -11,6 +11,8 @@ module Massalia.Auth (
 
 import Protolude
 
+import Web.JWT
+
 -- | A JWT claim encoded, the Token in its exchangeable format.
 -- No check is performed for integrity (no smart constructor).
 newtype JWTEncodedString = JWTEncodedString Text deriving (Eq, Show, Read, Generic)
