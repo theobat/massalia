27f43c7112ec4797868fc72b7ce0055a5cc26f0b
Diego Imbert <diegoimbert@pm.me>
2023-07-31 11:01:11 +0200

    Higher-kinded designed SchemaField and SchemaSelection

--- src/Massalia/RESTSchema.hs
+++ src/Massalia/RESTSchema.hs
@@ -10,97 +10,69 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DefaultSignatures #-}
 
+
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 -- |
 -- Module      : Massalia.RESTSchema
 -- Description : A module to define schemas for REST API that can be verified
 module Massalia.RESTSchema (
-  RestSchema (..), RestSchemaField (..),
-  RestSchemaValidateContext, RestSchemaValidateError (..),
-  Person (..), API (..), GenericIsObject (..),
-  restSchemaValidate
 ) where
+import Control.Monad.Identity (Identity)
 import Data.Text (Text)
-import Massalia.Utils (intercalate)
-import Protolude (Generic (Rep, to, from), Proxy (..), (:*:), M1, K1, Selector (selName), Alternative ((<|>)), Type, U1, Constructor)
-import Massalia.SelectionTree (JsonMassaliaTree, MassaliaTree (getName, getChildrenList), isLeaf)
-import qualified Data.Text as Text
-import GHC.Generics (S, D, C, R, (:+:), Constructor (conIsRecord))
-import Massalia.GenericUtils (proxySelName)
+import Data.Kind (Type)
 
-data RestSchemaField = RestSchemaLeaf | forall b. RestSchema b => RestSchemaObject (Proxy b)
+class Schema (f :: (Type -> Type) -> Type) where
+  getSchema :: f SchemaField
 
-class RestSchema a where
-  restSchemaLookupField :: Proxy a -> Text -> Maybe RestSchemaField
-  default restSchemaLookupField :: (Generic a, GRestSchema (Rep a)) => Proxy a -> Text -> Maybe RestSchemaField
-  restSchemaLookupField (_ :: Proxy a) = gRestSchemaLookupField (Proxy @(Rep a))
+data SchemaField a where
+  SFLeaf :: SchemaField (Identity a)
+  SFObject :: Schema a => (a SchemaField) -> SchemaField (Identity (a SchemaField))
+  SFList :: SchemaField (Identity a) -> SchemaField [a]
 
-class GRestSchema (f :: Type -> Type) where
-  gRestSchemaLookupField :: Proxy f -> Text -> Maybe RestSchemaField
+data SchemaSelection a where
+  SSFalse :: SchemaSelection (m a)
+  SSTrue :: SchemaSelection (m a)
+  SSSelectFields :: Schema a => a SchemaSelection -> SchemaSelection (m (a SchemaSelection))
 
-instance (GRestSchema a, GRestSchema b) => GRestSchema (a :*: b) where
-  gRestSchemaLookupField _ name =
-    gRestSchemaLookupField (Proxy @a) name <|> gRestSchemaLookupField (Proxy @b) name
-instance (GRestSchema a) => GRestSchema (M1 D s a) where
-  gRestSchemaLookupField _ = gRestSchemaLookupField (Proxy @a)
-instance (GRestSchema a) => GRestSchema (M1 C s a) where
-  gRestSchemaLookupField _ = gRestSchemaLookupField (Proxy @a)
-instance (Selector s) => GRestSchema (M1 S s (K1 R a)) where
-  gRestSchemaLookupField _ name
-    | fieldName == name = undefined
-    | otherwise = Nothing
-    where fieldName = Text.pack $ selName (proxySelName :: M1 S s _ p)
+-- EXAMPLE
 
-class GenericIsObject (f :: Type -> Type) where
-  gIsObject :: Proxy f -> Bool
-
-instance (GenericIsObject a) => GenericIsObject (M1 D s a) where
-  gIsObject _ = gIsObject (Proxy @a)
-instance (Constructor s) => GenericIsObject (M1 C s a) where
-  gIsObject _ = conIsRecord (proxySelName :: M1 C s a p)
-instance GenericIsObject (a :+: b) where
-  gIsObject _ = False
-
-
-restSchemaValidate :: RestSchema a =>
-  Proxy a -> RestSchemaValidateContext -> JsonMassaliaTree -> [RestSchemaValidateError]
-restSchemaValidate (Proxy :: Proxy a) oldCtx input =
-  case restSchemaLookupField (Proxy @a) (getName input) of
-    Nothing                                -> [mkErr oldCtx (getName input) "does not exist"]
-    Just RestSchemaLeaf | isLeaf input     -> []
-                        | otherwise        -> [mkErr ctx (getName input) "must be a leaf node"]
-    Just (RestSchemaObject s)
-      | not . null $ getChildrenList input -> getChildrenList input >>= restSchemaValidate s ctx
-      | otherwise                          -> [mkErr ctx (getName input) "has no fields specified"]
-  where
-    ctx = getName input : oldCtx
-
--- Each new context is added at the head of the list
-type RestSchemaValidateContext = [Text]
-
-data RestSchemaValidateError = RestSchemaValidateError {
-  vrs_errorMessage :: Text,
-  vrs_errorCtx :: RestSchemaValidateContext,
-  vrs_errorFieldName :: Text
-} deriving (Eq)
-instance Show RestSchemaValidateError where
-  show e = Text.unpack $
-    formatCtx (vrs_errorCtx e) <> ": '" <> vrs_errorFieldName e <> "' " <> vrs_errorMessage e
-
-data Person = Person { name :: Text, age :: Int }
-  deriving (Show, Eq, Generic)
-instance RestSchema Person
-
-data API = API {
-  person :: Person,
-  users :: Int
-} deriving (Show, Eq, Generic)
-instance RestSchema API
-
-mkErr :: RestSchemaValidateContext -> Text -> Text -> RestSchemaValidateError
-mkErr ctx fieldName message = RestSchemaValidateError{
-  vrs_errorMessage=message,
-  vrs_errorCtx=ctx,
-  vrs_errorFieldName=fieldName
+data Person f = Person {
+  name :: f (Identity Text),
+  age :: f (Identity Int),
+  email :: f (Identity Text),
+  favoriteNumbers :: f [Int]
 }
-formatCtx :: RestSchemaValidateContext -> Text
-formatCtx = intercalate "." . reverse
+
+data API f = API {
+  version :: f (Identity Int),
+  webMaster :: f (Identity (Person f)),
+  users :: f [Person f]
+}
+
+instance Schema API where
+  getSchema = API {
+    version = SFLeaf,
+    webMaster = SFObject getSchema,
+    users = SFList (SFObject getSchema)
+  }
+
+instance Schema Person where
+  getSchema = Person {
+    name = SFLeaf,
+    age = SFLeaf,
+    email = SFLeaf,
+    favoriteNumbers = SFList SFLeaf
+  }
+
+
+selection :: API SchemaSelection
+selection = API {
+  version = SSFalse,
+  webMaster = SSTrue,
+  users = SSSelectFields Person {
+    name = SSTrue,
+    age = SSFalse,
+    email = SSTrue,
+    favoriteNumbers = SSTrue
+  }
+}
