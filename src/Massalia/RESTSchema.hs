44d3693a1323476ccee94017d09dab699011bdda
Diego Imbert <diegoimbert@pm.me>
2023-08-01 17:01:51 +0200

    Transformer design

--- src/Massalia/RESTSchema.hs
+++ src/Massalia/RESTSchema.hs
@@ -13,50 +13,76 @@
 
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use newtype instead of data" #-}
+
 -- |
 -- Module      : Massalia.RESTSchema
 -- Description : A module to define schemas for REST API that can be verified
 module Massalia.RESTSchema (
 ) where
-import Control.Monad.Identity (Identity)
+import Control.Monad.Identity (IdentityT(IdentityT), Identity, (>=>))
 import Data.Text (Text)
-import Data.Kind (Type)
 import GHC.Generics (Generic)
 import Massalia.SelectionTree (JsonMassaliaTree)
 
-class Schema (a :: (Type -> Type) -> (Type -> Type) -> Type) where
+class Schema a where
   parseQuery :: JsonMassaliaTree -> a ScalarSelect ObjSelect
-  default parseQuery :: Generic (a ScalarSelect ObjSelect) => JsonMassaliaTree -> a ScalarSelect ObjSelect
-  parseQuery = undefined
 
-data ScalarSelect a = SSPick | SSDiscard
-data ObjSelect a where
-  SSSelect :: a ScalarSelect ObjSelect -> ObjSelect (rel (a ScalarSelect ObjSelect))
+data ScalarSelect m a = SPick | SDiscard
+  deriving (Generic, Functor)
 
-data Person scalar obj = Person {
-  name :: scalar Text,
-  age :: scalar Int,
-  favNumbers :: scalar [Int]
-} deriving (Generic)
+data ObjSelect m a = OPick a | ODiscard
+  deriving (Generic, Functor)
+
+
+-- Examples
 
 data API scalar obj = API {
-  version :: scalar Int,
-  admin :: obj (Identity (Person scalar obj)),
-  users :: obj [Person scalar obj]
+  version :: scalar Identity Int,
+  admin :: obj Identity (Person scalar obj),
+  users :: obj [] (Person scalar obj)
 } deriving (Generic)
 
+data Person scalar obj = Person {
+  name :: scalar Identity Text,
+  age :: scalar Maybe Int,
+  favNumbers :: scalar [] Int,
+  privateData :: obj Identity (PrivateData scalar obj)
+} deriving (Generic)
+
+newtype PrivateData scalar obj = PrivateData {
+  password :: scalar Identity Text
+} deriving (Generic)
+
+db :: API IdentityT IdentityT
+db = API {
+  version = pure 20,
+  admin = pure Person {
+    name = pure "Diego",
+    age = pure 20,
+    favNumbers = IdentityT [-1, 2, 42, 64],
+    privateData = pure PrivateData {
+      password = pure "myPassword"
+    }
+  },
+  users = IdentityT [
+    -- Other users...
+  ]
+}
+
+-- Transformers are easily composable
+adminAge :: API _ _ -> IdentityT _ Text
+adminAge = admin >=> privateData >=> password
+ 
 selection :: API ScalarSelect ObjSelect
 selection = API {
-  version = SSPick,
-  admin = SSSelect Person {
-    name = SSPick,
-    age = SSDiscard,
-    favNumbers = SSPick
-  },
-  users = SSSelect Person {
-    name = SSPick,
-    age = SSDiscard,
-    favNumbers = SSDiscard
+  version = SPick,
+  admin = ODiscard,
+  users = OPick Person {
+    name = SPick,
+    age = SDiscard,
+    favNumbers = SPick,
+    privateData = OPick PrivateData {
+      password = SPick
+    }
   }
 }
