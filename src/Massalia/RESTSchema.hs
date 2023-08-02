543e8e3a225a2c96c22a0d84521ecdb6284b3e46
Diego Imbert <diegoimbert@pm.me>
2023-08-02 12:37:10 +0200

    added error context

--- src/Massalia/RESTSchema.hs
+++ src/Massalia/RESTSchema.hs
@@ -36,9 +36,10 @@ import Control.Applicative (Applicative(liftA2))
 import Massalia.Utils (intercalate)
 
 class Schema a where
-  parseQuery :: JsonMassaliaTree -> Either ParseQueryError (Selection a)
-  default parseQuery :: (Generic (Selection a), GSchema (Rep (Selection a))) => JsonMassaliaTree -> Either ParseQueryError (Selection a)
-  parseQuery = fmap to . gParseQuery Proxy
+  parseQuery :: Context -> JsonMassaliaTree -> Either ParseQueryError (Selection a)
+  default parseQuery :: (Generic (Selection a), GSchema (Rep (Selection a))) =>
+    Context -> JsonMassaliaTree -> Either ParseQueryError (Selection a)
+  parseQuery ctx = fmap to . gParseQuery Proxy ctx
 
 type Selection a = a ScalarSelect ObjSelect
 type Value a = a IdentityT IdentityT
@@ -49,12 +50,13 @@ data ParseQueryError = PQError {
 }
 instance Show ParseQueryError where
   show PQError{pqe_ctx, pqe_msg} = Text.unpack $
-    "root" <> intercalate "." (reverse pqe_ctx) <> ": " <> pqe_msg
-mkFieldErr :: [Text] -> Text -> Text -> ParseQueryError
+    intercalate "." ("root" : reverse pqe_ctx) <> ": " <> pqe_msg
+mkFieldErr :: Context -> Text -> Text -> ParseQueryError
 mkFieldErr ctx field msg = PQError {
   pqe_msg = "'" <> field <> "'" <> " " <> msg,
   pqe_ctx = ctx
 }
+type Context = [Text]
 
 data ScalarSelect m a = SPick | SDiscard
   deriving (Generic, Functor, Show)
@@ -65,26 +67,26 @@ data ObjSelect m a = OPick a | ODiscard
 -- Generic code
 
 class GSchema f where
-  gParseQuery :: Proxy (f a) -> JsonMassaliaTree -> Either ParseQueryError (f a)
+  gParseQuery :: Proxy (f a) -> Context -> JsonMassaliaTree -> Either ParseQueryError (f a)
 
 instance GSchema a => GSchema (M1 D s a) where
-  gParseQuery _ = fmap M1 . gParseQuery (Proxy @(a _))
+  gParseQuery _ ctx = fmap M1 . gParseQuery (Proxy @(a _)) ctx
 instance GSchema a => GSchema (M1 C s a) where
-  gParseQuery _ = fmap M1 . gParseQuery (Proxy @(a _))
+  gParseQuery _ ctx = fmap M1 . gParseQuery (Proxy @(a _)) ctx
 instance (Selector s) => GSchema (M1 S s (K1 i (ScalarSelect m a))) where
-  gParseQuery _ t = M1 . K1 <$> case lookupChildren fieldName t of
+  gParseQuery _ ctx t = M1 . K1 <$> case lookupChildren fieldName t of
       Nothing            -> Right SDiscard
       Just x | isLeaf x  -> Right SPick
-             | otherwise -> Left (mkFieldErr [] fieldName "must be a leaf node")
+             | otherwise -> Left (mkFieldErr ctx fieldName "must be a leaf node")
     where fieldName = Text.pack $ selName (proxySelName :: (M1 S s _ _))
 instance (Selector s, Schema a) => GSchema (M1 S s (K1 i (ObjSelect m (Selection a)))) where
-  gParseQuery _ t = M1 . K1 <$> case lookupChildren fieldName t of
+  gParseQuery _ ctx t = M1 . K1 <$> case lookupChildren fieldName t of
       Nothing            -> Right ODiscard
-      Just x | isLeaf x  -> Left (mkFieldErr [] fieldName "cannot be a leaf node, must specify children")
-             | otherwise -> OPick <$> parseQuery x
+      Just x | isLeaf x  -> Left (mkFieldErr ctx fieldName "cannot be a leaf node, must specify children")
+             | otherwise -> OPick <$> parseQuery (fieldName : ctx) x
     where fieldName = Text.pack $ selName (proxySelName :: (M1 S s _ _))
 instance (GSchema a, GSchema b) => GSchema (a :*: b) where
-  gParseQuery _ t = liftA2 (:*:) (gParseQuery (Proxy @(a _)) t) (gParseQuery (Proxy @(b _)) t)
+  gParseQuery _ ctx t = liftA2 (:*:) (gParseQuery (Proxy @(a _)) ctx t) (gParseQuery (Proxy @(b _)) ctx t)
 
 
 -- Examples
